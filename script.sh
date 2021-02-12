2bb35d0886fc24213c0670977b11b900d9b31225
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2021-02-12 12:49:27 +0530

    added a bash script

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+all=()
+for i in $(git ls-remote --heads origin_shruti);
+do
+all+=($i);
+done
+
+length=${#all[@]}
+
+sha=()
+for ((i=0; i<$length; i++));
+do
+if (($i%2==0));
+then sha+=(${all[$i]});
+fi;
+done
+
+#for i in ${sha[@]}
+#do echo $i
+#done
+echo "::set-output name=TAG_NAME"::$sha
