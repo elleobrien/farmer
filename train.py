19610d82bd297bde4412c81e06b99b5149450cb4
hhurchand <h.hurchand@gmail.com>
2021-07-12 11:09:45 -0400

    Create train.py

--- train.py
+++ train.py
@@ -1,24 +1,49 @@
 import pandas as pd 
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve
+
 from sklearn.model_selection import train_test_split
 import json
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
-import pickle
 
-###
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+import os
+import lightgbm as lgb
+import neptune
+
+from neptunecontrib.monitoring.lightgbm import neptune_monitor
+
+import numpy as np
+import pandas as pd
+
+import pickle
+ 
+import logging
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+# In[2]:
+neptune.init(api_token=os.getenv('NEPTUNE_API_TOKEN'),
+            project_qualified_name=os.getenv('NEPTUNE_PROJECT_NAME'))
 
 df = pd.read_csv('BostonData.csv',header=0)
+#df.to_csv(r'C:\Users\hhurc\BostonData\BostonData.csv')
 
 
+# ## EDA
+
+# In[3]:
 
 
+df.head(5)
 
 
 # In[4]:
@@ -36,12 +61,22 @@ df_correl = df.corr()
 #sns.heatmap(df_correl,annot=True)
 
 
-sns.set_color_codes("dark")
-ax = sns.heatmap(df_correl,annot=True)
-plt.savefig("by_region.png",dpi=80)
+# In[6]:
 
 
+#df.info()
 
+
+# In[7]:
+
+
+#sns.pairplot(df)
+
+
+# In[8]:
+
+
+#standardize predictors
 from sklearn.preprocessing import StandardScaler
 
 
@@ -87,7 +122,15 @@ y = df0["MEDV"]
 # In[15]:
 
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.5)
+
+
+neptune.init(api_token=os.getenv('NEPTUNE_API_TOKEN'),
+            project_qualified_name=os.getenv('NEPTUNE_PROJECT_NAME'))
+
+neptune.create_experiment('BostonData-NEPTUNE')
+
+
 
 
 # In[16]:
@@ -96,6 +139,8 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
 from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
 model = reg.fit(X_train,y_train)
+pickle.dump(model,open('model.pkl','wb'))
+
 y_pred = model.predict(X_test)
 
 from sklearn.metrics import mean_squared_error, r2_score
@@ -104,7 +149,32 @@ mse = mean_squared_error(y_test, y_pred, squared=False)
 rmse = math.sqrt(mse)
 r2 = r2_score(y_test, y_pred)
 
+neptune.log_metric('mse',mse)
+neptune.log_metric('rmse', rmse)
+neptune.log_metric('r2', r2)
 # In[17]:
+
+
+print("y-intercept",model.coef_[0])
+
+
+# In[18]:
+
+## UNCOMMENT FOR MLFLOW REPORTING
+#mlflow.set_experiment(experiment_name="experiment1")
+#mlflow.set_tracking_uri("http://localhost:5000")
+#with mlflow.start_run():
+#    mlflow.log_param("alpha1",model.coef_[0])
+#    mlflow.log_param("beta1",model.coef_[1])
+
+
+# In[ ]:
+
+if os.getenv('CI') == "true":
+    neptune.append_tag('ci-pipeline', os.getenv('NEPTUNE_EXPERIMENT_TAG_ID'))
+
+
+
 
 
 with open("metrics.json", 'w') as outfile:
