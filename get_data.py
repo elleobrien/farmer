482d33b9f5b91a06261a1ec6e77ac113a9b78b57
jake rutherford <jake.rutherford@LTP007569.local>
2022-11-16 11:29:12 +0000

    setup dvc pipelines

--- get_data.py
+++ get_data.py
@@ -1,18 +1,21 @@
 import os
 import wget
 
-# data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
+# # data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
 
-# Download the zipped dataset
-url = 'https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/yshdbyj6zy-1.zip'
-zip_name = "data.zip"
-wget.download(url, zip_name)
+# # Download the zipped dataset
+# url = 'https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/yshdbyj6zy-1.zip'
+# zip_name = "data.zip"
+# wget.download(url, zip_name)
 
-# Unzip it and standardize the .csv filename
-import zipfile
-with zipfile.ZipFile(zip_name,"r") as zip_ref:
-    zip_ref.filelist[0].filename = 'data_raw.csv'
-    zip_ref.extract(zip_ref.filelist[0])
+# # Unzip it and standardize the .csv filename
+# import zipfile
+# with zipfile.ZipFile(zip_name,"r") as zip_ref:
+#     zip_ref.filelist[0].filename = 'data_raw.csv'
+#     zip_ref.extract(zip_ref.filelist[0])
 
-os.remove(zip_name)
+# os.remove(zip_name)
 
+url = "https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/383116/rawdata_new.csv?sequence=1&isAllowed=y"
+
+wget.download(url, "data_raw.csv")
